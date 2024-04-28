79d1338bbf80828b11b91f1a8847098c1a0ee73e
A00837527 <A00837527@tec.mx>
2024-04-27 19:02:30 -0600

    eougerbpigyb

--- nerisbloque-app/src/API/FirebaseConfig.js
+++ nerisbloque-app/src/API/FirebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from "firebase/app";
 import {getAuth, setPersistence, browserLocalPersistence} from"firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
