c18f411c4402ffc25f698113393d7bc186dd1e77
Emilio Vidal <evpaez@gmail.com>
2024-05-01 00:05:30 -0600

    admin w

--- nerisbloque-app/src/components/AdminView.jsx
+++ nerisbloque-app/src/components/AdminView.jsx
@@ -6,7 +6,7 @@ import NeorisPP from '../img/NEORIS logo light.png'
 import './AdminView.css'
 
 
-function AdminView({nombreU}) {
+function AdminView() {
  
   return (
     <div>
@@ -14,10 +14,6 @@ function AdminView({nombreU}) {
                 <img src={NeorisPP} alt="foto de la compañia" /><span>ADMIN</span>
         </div>
         <h2 id="admin-title">Panel de Administración</h2>
-      <div id='panel-Admin'>
-        <h3>Nombre del Administrador:</h3>
-        <h4>{nombreU}</h4>
-      </div>
       <div id='tabla'>
         <UserList />
         <div id="mid-line"></div>
